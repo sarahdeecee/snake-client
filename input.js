2d0b4b7b9247df275e48c66803efe695f21753d6
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-11-03 15:58:38 -0400

    wasd inputs send movement commands to server

--- input.js
+++ input.js
@@ -1,38 +1,45 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
+  const handleUserInput = function () {
+    const stdin = process.stdin;
+    
+    const up = () => conn.write('Move: up');
+    const down = () => conn.write('Move: down');
+    const left = () => conn.write('Move: left');
+    const right = () => conn.write('Move: right');
+    
+    stdin.on('data', (key) => {
+      // process.stdout.write('.');
+      if (key === '\u0003') {
+        process.exit();
+      }
+      if (key === 'w') {
+        up();
+        console.log('up');
+      }
+      if (key === 's') {
+        down();
+        console.log('down');
+      }
+      if (key === 'a') {
+        left();
+        console.log('left');
+      }
+      if (key === 'd') {
+        right();
+        console.log('right');
+      }
+    });
+  };
   stdin.on("data", handleUserInput);
   stdin.resume();
   return stdin;
 };
 
-const handleUserInput = function () {
-  const stdin = process.stdin;
 
-  const up = () => stdin.write('Move: up');
-  const down = () => stdin.write('Move: down');
-  const left = () => stdin.write('Move: left');
-  const right = () => stdin.write('Move: right');
-
-  stdin.on('data', (key) => {
-    // process.stdout.write('.');
-    if (key === '\u0003') {
-      process.exit();
-    }
-    if (key === 'w') {
-      up();
-    }
-    if (key === 's') {
-      down();
-    }
-    if (key === 'a') {
-      left();
-    }
-    if (key === 'd') {
-      right();
-    }
-  });
-};
-
-module.exports = { setupInput, handleUserInput };
+module.exports = { setupInput };
