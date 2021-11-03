39318c5314aef6175dacbeb459c04ea5e5357c9d
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-11-03 16:10:39 -0400

    moved constants to constants.js

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const { time } = require("console");
+const { IP, PORT } = require('./constants');
 const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
